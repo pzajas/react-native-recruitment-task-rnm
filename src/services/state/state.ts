2fa0f9f51ebb1af191caafc8c6e76311e876dd09
pzajas <zajas.piotr@gmail.com>
2024-12-03 12:21:25 +0100

    Add async storage to jotai to preserve tehe state.

--- src/services/state/state.ts
+++ src/services/state/state.ts
@@ -1,4 +1,17 @@
-import {atom} from 'jotai';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {atomWithStorage} from 'jotai/utils';
 
-export const currentPageAtom = atom(1);
-export const favoritesAtom = atom([]);
+const asyncStorage = {
+  getItem: async key => {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  },
+  setItem: async (key, value) => {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  },
+  removeItem: async key => {
+    await AsyncStorage.removeItem(key);
+  },
+};
+
+export const favoritesAtom = atomWithStorage('favorites', [], asyncStorage);
